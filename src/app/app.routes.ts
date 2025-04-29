19a420c68399b5f1963ae66b833c44058b38e5c9
pabloj19 <pablo_jmz1@hotmail.com>
2025-04-29 16:34:27 -0500

    ruta carrito agregada

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { PayphoneFormComponent } from './pasarela-pago/payphone-form/payphone-form.component';
 import { CarritoComponent } from './pasarela-pago/carrito/carrito.component';
 import { RespuestaPagoComponent } from './pasarela-pago/respuesta-pago/respuesta-pago.component';
+import { WelcomeComponent } from './pages/welcome/welcome.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'carrito', pathMatch: 'full' },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+    {path: 'home', component: WelcomeComponent},
     { path: 'carrito', component: CarritoComponent },
     { path: 'pago', component: PayphoneFormComponent },
     { path: 'respuesta-pago', component: RespuestaPagoComponent },
