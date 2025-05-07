1ddb6facd34b8f9a8d6afaa9c4e6c763653a5419
chente09 <chentenengerc@gmail.com>
2025-05-07 11:19:45 -0500

    mejoras estilos v2

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -1,13 +1,12 @@
-import { Component, HostListener, OnInit } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { NzDropDownModule, NzPlacementType } from 'ng-zorro-antd/dropdown';
+import { NzDropDownModule } from 'ng-zorro-antd/dropdown';
 import { NzIconModule } from 'ng-zorro-antd/icon';
 import { NzBadgeModule } from 'ng-zorro-antd/badge';
-import { NavigationEnd, Router, RouterLink, RouterModule } from '@angular/router';
+import { Router, RouterLink, RouterModule } from '@angular/router';
 import { NzGridModule } from 'ng-zorro-antd/grid';
 import { NzButtonModule } from 'ng-zorro-antd/button';
 import { NzMenuModule } from 'ng-zorro-antd/menu';
-import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-nav-bar',
@@ -30,28 +29,22 @@ export class NavBarComponent {
   mobileMenuOpen = false;
   cartCount = 1;
   isScrolled = false;
+  hideHeader = false;
+  lastScrollTop = 0;
   selectedLanguage = { name: 'Ecuador (ES)' };
 
-  // Menú de idiomas
   languages = [
     { code: 'es_ES', name: 'Ecuador (ES)' },
     { code: 'en_US', name: 'United States (EN)' },
   ];
 
-  // Menú de navegación principal
   navItems = [
     { label: 'NOSOTROS', link: '/nosotros' },
     { label: 'PRODUCTOS', link: '/productos' },
     { label: 'RESEÑAS', link: '/resenas' }
-  ]; 
+  ];
 
-  constructor(private router: Router) {
-    // Escuchar eventos de scroll
-    window.addEventListener('scroll', () => {
-      this.isScrolled = window.scrollY > 50;
-    });
-  }
-
+  constructor(private router: Router) {}
 
   toggleMobileMenu() {
     this.mobileMenuOpen = !this.mobileMenuOpen;
@@ -61,11 +54,23 @@ export class NavBarComponent {
     this.selectedLanguage = language;
   }
 
-  // Cerrar menú al hacer clic en un enlace en móvil
   closeMenuOnMobile() {
     if (window.innerWidth <= 768) {
       this.mobileMenuOpen = false;
     }
   }
-  
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    const currentScroll = window.pageYOffset || document.documentElement.scrollTop;
+    this.isScrolled = currentScroll > 50;
+
+    if (currentScroll > this.lastScrollTop && currentScroll > 100) {
+      this.hideHeader = true; // Scrolling down
+    } else {
+      this.hideHeader = false; // Scrolling up
+    }
+
+    this.lastScrollTop = currentScroll <= 0 ? 0 : currentScroll;
+  }
 }
